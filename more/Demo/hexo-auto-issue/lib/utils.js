e49b906e97c674d0187bff725ebf41780e6d29e9
holdyounger <88082435+holdyounger@users.noreply.github.com>
2024-01-23 18:48:34 +0800

    Site updated: 2024-01-23 18:47:59

--- more/Demo/hexo-auto-issue/lib/utils.js
+++ more/Demo/hexo-auto-issue/lib/utils.js
@@ -4,43 +4,9 @@ const path = require('path');
 const crypto = require('crypto');
 const nunjucks = require('nunjucks');
 
-const TPL_TITLE = '';
-const TPL_BODY = ''use strict';
-const fs = require('fs');
-const path = require('path');
-const crypto = require('crypto');
-const nunjucks = require('nunjucks');
-
 const TPL_TITLE = '{{ title }}';
 const TPL_BODY = '{{ _content }}';
 const TPL_LABELS = "{{ tags.data|join(',', 'name') }},{{ categories.data|join(',', 'name') }}";
-
-function issueDataBuilder(item, hexo) {
-    const issueData = {};
-    const {
-        template: {body = TPL_BODY, title = TPL_TITLE, labels = TPL_LABELS} = {},
-        withGitalk
-    } = hexo.config.auto_issue || {};
-    issueData.title = nunjucks.renderString(title, {...item});
-    issueData.body = nunjucks.renderString(body, {...item});
-    let allLabels = nunjucks.renderString(labels, {...item});
-    allLabels = Array.from(new Set(allLabels.split(',').filter(item => !!item)));
-    let gitalkId;
-    if (withGitalk) {
-        const url_for = hexo.extend.helper.get('url_for').bind(hexo);
-        const str = url_for(item.path);
-        // gitalkId = crypto.createHash('md5').update(str).digest('hex');
-        // allLabels.push(gitalkId);
-        allLabels.push('Gitalk');
-    }
-    issueData.labels = allLabels;
-    return {issueData, gitalkId};
-}
-
-module.exports = {
-    issueDataBuilder,
-}';
-const TPL_LABELS = ",";
 
 function issueDataBuilder(item, hexo) {
     const issueData = {};
